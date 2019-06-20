05b8870672bb32be2c9bef7f752885ce119991ac
tcw57 <skylerskyler732@gmail.com>
2019-06-19 21:23:36 -0400

    almost there

--- remove.py
+++ remove.py
@@ -2,3 +2,10 @@ import os
 for i in range(27):
 	x = 'phi'+str(i)+'.dat';
 	os.remove(x);
+for i in range(9):
+	x = 'phi'+str(i)+'_p.dat';
+	os.remove(x);
+for i in range(9):
+	x = 'phi'+str(i)+'_n.dat';
+	os.remove(x);
+
